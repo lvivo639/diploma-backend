d4ededb89c17f57522c93d0367430f8185290b05
Volodymyr Lomaka <volodymyr.lomaka@voypost.com>
2021-06-11 12:07:29 +0300

    password

---
+++ extensions/users-permissions/controllers/User.js
@@ -0,0 +1,45 @@
+"use strict";
+
+/**
+ * Auth.js controller
+ *
+ * @description: A set of functions called "actions" for managing `Auth`.
+ */
+
+/* eslint-disable no-useless-escape */
+module.exports = {
+  async settings(ctx) {
+    const { firstName, lastName, email } = ctx.request.body;
+
+    if (!firstName) return ctx.badRequest("firstName is required");
+    if (!lastName) return ctx.badRequest("lastName is required");
+    if (!email) return ctx.badRequest("email is required");
+
+    return await strapi
+      .query("user", "users-permissions")
+      .update({ id: ctx.state.user.id }, { firstName, lastName, email });
+  },
+
+  async changePassword(ctx) {
+    const { password, newPassword } = ctx.request.body;
+
+    if (!password) return ctx.badRequest("password is required");
+    if (!newPassword) return ctx.badRequest("newPassword is required");
+
+    const valid = await strapi.plugins[
+      "users-permissions"
+    ].services.user.validatePassword(password, ctx.state.user.password);
+
+    if (!valid) return ctx.badRequest("password is not valid");
+
+    const updatedPassword = await strapi.plugins[
+      "users-permissions"
+    ].services.user.hashPassword({
+      password: newPassword,
+    });
+
+    return await strapi
+      .query("user", "users-permissions")
+      .update({ id: ctx.state.user.id }, { updatedPassword });
+  },
+};
